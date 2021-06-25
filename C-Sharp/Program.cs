c4888324164696d70d69f1ade730c850dc52535e
CaptinBata <nbata1997@gmail.com>
2021-06-25 19:39:35 +0100

    Added start of C# version

---
+++ C-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ventillo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C-Sharp/engine.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace Ventillo
+{
+    class Engine
+    {
+
+    }
+}
