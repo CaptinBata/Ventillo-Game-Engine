ec6ad1e4de8b4136d8a167396460bece8b9455e5
CaptinBata <nbata1997@gmail.com>
2021-06-26 21:50:03 +0100

    Changed namespaces and includes

--- C-Sharp/Ventillo/debug/DebugObject.cs
+++ C-Sharp/Ventillo/debug/DebugObject.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ventillo.GameObjects;
+using Ventillo.Utils;
 
 namespace Ventillo.Debug
 {
--- C-Sharp/Ventillo/utils/Vector.cs
+++ C-Sharp/Ventillo/utils/Vector.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ventillo
+namespace Ventillo.Utils
 {
     public class Vector
     {
