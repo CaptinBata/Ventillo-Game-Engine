71bd37feb27339681825bb2e8584ef8ab8d4035c
CaptinBata <nbata1997@gmail.com>
2021-06-26 21:47:17 +0100

    Reorganised structure to be correct layout with library

---
+++ C-Sharp/Ventillo/debug/DebugObject.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventillo.GameObjects;
+
+namespace Ventillo.Debug
+{
+    class DebugObject
+    {
+        private int LastTime = 0;
+        private int FPS = 0;
+
+        DebugObject(Vector position, MinMax playableArea)
+        {
+            double xMidPosition = playableArea.Min.x + ((playableArea.Max.x - playableArea.Min.x) / 2);
+            double yMidPosition = playableArea.Min.y + ((playableArea.Max.y - playableArea.Min.y) / 2);
+        }
+    }
+}
